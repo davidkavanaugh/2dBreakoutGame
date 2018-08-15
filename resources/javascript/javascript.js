e44c27669cf8e57a8088a713f5f49c1c85d146f6
David Kavanaugh <david.kavanaugh@me.com>
2018-08-15 07:05:54 -0700

    added bricks and refined ball bounces

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -1,15 +1,30 @@
-let canvas = document.getElementById("gameCanvas");
-let ctx = canvas.getContext("2d");
-let ballRadius = 2.5;
-let x = canvas.width/2;
-let y = canvas.height-30;
-let dx = 2;
-let dy = -2;
-let paddleHeight = 5;
-let paddleWidth = 50;
-let paddleX = (canvas.width-paddleWidth)/2;
-let rightPressed = false;
-let leftPressed = false;
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 2.5;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -45,17 +60,44 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 }
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#fff";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+           if(y= y-paddleHeight){
+            dy = -dy  ;
+			 }
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
     }
 
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
@@ -69,4 +111,4 @@ function draw() {
     y += dy;
 }
 
-setInterval(draw, 20);
+setInterval(draw, 10);
